2419c1f371256dfe9f796563d5285769016ca4b0
phdgamer <pedro380dalpa@gmail.com>
2021-03-06 20:42:29 -0300

    alter head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-
+          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
         </Head>
         <body>
           <Main />
