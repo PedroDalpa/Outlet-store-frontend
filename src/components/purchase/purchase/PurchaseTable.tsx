7bd25300d50ded41fa6d9d839f54dea21c37c9bd
phdgamer <pedro380dalpa@gmail.com>
2021-03-25 22:28:04 -0300

    generate tags when do a purchase

--- src/components/purchase/purchase/PurchaseTable.tsx
+++ src/components/purchase/purchase/PurchaseTable.tsx
@@ -1,5 +1,7 @@
+import { BarcodeOutlined } from '@ant-design/icons';
 import { Table } from 'antd';
 import { ColumnsType } from 'antd/es/table';
+import Link from 'next/link';
 import { useContext } from 'react';
 import { PurchaseContext } from '../../../contexts/purchase/PurchaseContext';
 
@@ -30,6 +32,21 @@ const columns: ColumnsType<Purchases> = [
     key: 'createAt',
     title: 'Criado em:',
     dataIndex: 'createAt',
+  },
+  {
+    title: 'Operação',
+    dataIndex: 'operacao',
+    align: 'center',
+
+    render: (text, record) => (
+      <>
+
+        <a target="_blank" href={`/product-tag/${record.id}`} style={{ textDecoration: 'none', color: 'black' }} rel="noopener noreferrer">
+          <BarcodeOutlined style={{ marginLeft: 20 }} />
+        </a>
+
+      </>
+    ),
   },
 
 ];
---
+++ src/pages/product-tag/[id].tsx
@@ -0,0 +1,54 @@
+import BarCode from 'react-barcode';
+import { GetServerSideProps } from 'next';
+import api from '../../services/api';
+import styles from '../../styles/pages/product-tag/Tags.module.css';
+
+export default function Product({ tags }) {
+  return (
+    <div className={styles.etiquetas}>
+      {tags.map((tag) => (
+        <span>
+          <div className={styles.etiqueta}>
+            <h1>{tag.name}</h1>
+
+            <BarCode
+              value={tag.barCode}
+              textPosition="top"
+              width={1.2}
+              height={28}
+              fontSize={12}
+            />
+          </div>
+        </span>
+
+      ))}
+
+    </div>
+
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { token } = context.req.cookies;
+  const { id } = context.params;
+
+  try {
+    const { data } = await api.get(`product/tag/${id}`, {
+      headers:
+      { authorization: token },
+    });
+
+    return {
+      props: {
+        tags: data,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        tags: 'ocorreu um erro!',
+      },
+    };
+  }
+};
